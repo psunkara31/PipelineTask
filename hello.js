9c69fcad71a8cf58a5ea47fb486c939f41bc3e19
Poojita Sunkara <psunkara@in.ibm.com>
2017-10-04 13:23:27 +0530

    sp commit

--- hello.js
+++ hello.js
@@ -1,10 +1,12 @@
 var express= require('express');
 var app = express()
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv(); 
 
 app.get('/',function(req,res)){
         
         res.send('hello world')
         }
-app.listen(8080,function){
-console.log("to port 8080")
-}
+app.listen(appEnv.port, '0.0.0.0', function() { 
+    console.log("server starting on " + appEnv.url);
+});
